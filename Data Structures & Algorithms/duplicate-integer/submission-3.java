ac57b18c85ebdfc8a349963159561afad7ab623b
Siraz22 <45779117+Siraz22@users.noreply.github.com>
2026-09-20 18:33:39 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,17 @@
+/*
+"Nothing can die while we are here"
+- Daydreamer, Aurora
+*/
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i =0;i<nums.length;++i){
+            if(set.contains(nums[i]))
+                return true;
+            
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
